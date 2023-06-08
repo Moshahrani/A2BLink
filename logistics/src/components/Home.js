4232bfda590940898b64cf047ff7a7c2ac62c0ab
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-08 15:53:51 -0700

    added video background for opening statement area

--- logistics/src/components/Home.js
+++ logistics/src/components/Home.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../css/Home.css";
+import Cargo from "../assets/Cargo.mp4";
 
 const Home = () => {
+  const videoRef = useRef();
+
+  // video should autoplay across all devices and screen sizes
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, []);
+
+  const contentStyles = {
+    position: "relative",
+    zIndex: 2,
+  };
+
+  const styles = {
+    position: "relative",
+    overflow: "hidden",
+    height: "90vh",
+  };
+
+  const videoStyles = {
+    position: "absolute",
+    width: "100%",
+    left: "50%",
+    top: "50%",
+    height: "100%",
+    objectFit: "cover",
+    transform: "translate(-50%, -50%)",
+    zIndex: "-1",
+  };
+
   return (
     <div>
-      <div className="intro-container">
-        <div className="opening-statement">
+      <div style={styles}>
+        <video
+          ref={videoRef}
+          src={Cargo}
+          autoPlay="autoplay"
+          playsInLine="playsinline"
+          loop="true"
+          muted="true"
+          style={videoStyles}
+        />
+        <div className="intro-container">
+          <div className="opening-statement">
             <h1 className="statement">We Keep Your Freight Moving!</h1>
             <h2 className="mission-statement">
               Access to thousands of qualified carriers!
             </h2>
           </div>
+        </div>
       </div>
     </div>
   );
