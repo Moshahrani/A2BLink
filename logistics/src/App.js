efcf410fd2b58d0e64cc2a26a3a9ad1f50dbbfda
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-08 08:10:18 -0700

    added routing for all nav links/components using react-router in app component

--- logistics/src/App.js
+++ logistics/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CustomNavbar from ".//components/CustomNavbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Shipping from "./components/Shipping";
+import Lead from "./components/Lead";
+import Contact from "./components/Contact";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <CustomNavbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <About />
+              </>
+            }
+          />
+          <Route
+            path="/shipping"
+            element={
+              <>
+                <Shipping />
+              </>
+            }
+          />
+          <Route
+            path="/lead"
+            element={
+              <>
+                <Lead />
+              </>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <>
+                <Contact />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
