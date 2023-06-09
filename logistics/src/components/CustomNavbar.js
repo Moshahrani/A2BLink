78d39c353eb6391497d40b4b724055e848cdaf54
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-09 10:31:20 -0700

    added opacity styling when scrolling down for navbar to become more transparent as user continues to scroll down screen

--- logistics/src/components/CustomNavbar.js
+++ logistics/src/components/CustomNavbar.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import "../css/CustomNavbar.css";
 import resized from "../assets/resized.png";
 
-
 const CustomNavbar = () => {
   const [expanded, setExpanded] = useState(false);
   const [opacity, setOpacity] = useState(1);
@@ -13,7 +12,7 @@ const CustomNavbar = () => {
     const handleScroll = () => {
       let scrolled = document.scrollingElement.scrollTop;
       if (scrolled > 50) { 
-        setOpacity(0.7);
+        setOpacity(0.6);
       } else {
         setOpacity(1);
       }
