1bec122d06dfc22dcdeafe928671a2f07c7c74fb
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-10 07:13:33 -0700

    formatted fourpoints component and put span tag above image tags for better placement of each subsection in component

--- logistics/src/components/FourPoints.js
+++ logistics/src/components/FourPoints.js
@@ -9,43 +9,47 @@ const FourPoints = () => {
     <div>
       <div className="fourstep-container">
         <div className="fourstep-main-text">
-          One unified marketplace. Various modes for what you need.
-        </div>
-        <div className="info-row">
-          <div className="info-container">
-            <img className="four-image" src={semi} alt="Image 1" />
-            <span>Full Truckload</span>
-            <p>
-              "Efficient and cost-effective shipping solution with exclusive use
-              of a full truckload for your cargo."
-            </p>
-          </div>
-          <div className="info-container">
-            <img className="four-image" src={boxtruck} alt="Image 2" />
-            <span>Less Than Truckload</span>
-            <p>
-              "Flexible shipping option for smaller loads, sharing truck space
-              and costs with other shipments."
-            </p>
+          <div className="fourstep-text">
+            One unified marketplace. Various modes for what you need.
           </div>
         </div>
-        <div className="info-row">
-          <div class="info-container">
-            <img className="four-image" src={air} alt="Image 3" />
-            <span>Air Freight</span>
-            <p>
-              "Swift and reliable transportation via air for time-sensitive
-              shipments across domestic and international destinations."
-            </p>
+        <div className="info-box">
+          <div className="info-row">
+            <div className="info-container">
+              <span>Full Truckload</span>
+              <img className="four-image" src={semi} alt="Image 1" />
+              <p>
+                "Efficient and cost-effective shipping solution with exclusive
+                use of a full truckload for your cargo."
+              </p>
+            </div>
+            <div className="info-container">
+              <span>Less Than Truckload</span>
+              <img className="four-image" src={boxtruck} alt="Image 2" />
+              <p>
+                "Flexible shipping option for smaller loads, sharing truck space
+                and costs with other shipments."
+              </p>
+            </div>
           </div>
-          <div className="info-container">
-            <img className="four-image" src={intermodal} alt="Image 4" />
-            <span>Intermodal</span>
-            <p>
-              "Versatile shipping solution that seamlessly combines multiple
-              modes of transport, maximizing efficiency and cost-effectiveness
-              for your cargo."
-            </p>
+          <div className="info-row">
+            <div className="info-container">
+              <span>Air Freight</span>
+              <img className="four-image" src={air} alt="Image 3" />
+              <p>
+                "Swift and reliable transportation via air for time-sensitive
+                shipments across domestic and international destinations."
+              </p>
+            </div>
+            <div className="info-container">
+              <span>Intermodal</span>
+              <img className="four-image" src={intermodal} alt="Image 4" />
+              <p>
+                "Versatile shipping solution that seamlessly combines multiple
+                modes of transport, maximizing efficiency and cost-effectiveness
+                for your cargo."
+              </p>
+            </div>
           </div>
         </div>
       </div>
