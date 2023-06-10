f6d73d26c60c5c16cdf8988f1df360044d460076
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-10 16:19:32 -0700

    created new contact component with name, email, phone, message options

--- logistics/src/components/Contact.js
+++ logistics/src/components/Contact.js
@@ -1,8 +1,29 @@
-const Contact = () => {
-    return (
-      <div>
-      </div>
-    );
-  };
-  
-  export default Contact;
+import "../css/Contact.css";
+
+function Contact() {
+  return (
+    <div className="form-container">
+      <form className="contact-form">
+        <label>
+          Name:
+          <input className="form-input" type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input className="form-input" type="email" name="email" />
+        </label>
+        <label>
+          Phone:
+          <input className="form-input" type="phone" name="phone" />
+        </label>
+        <label className="message">
+          Message:
+          <textarea className="form-input" name="message" />
+        </label>
+        <input className="contact-button" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
