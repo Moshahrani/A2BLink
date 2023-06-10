8bd0150969305f5bc8e90c5b7daa2b6ec31e95e0
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-10 16:18:36 -0700

    added new footer component to app.js for routing to all routes

--- logistics/src/App.js
+++ logistics/src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Shipping from "./components/Shipping";
 import Lead from "./components/Lead";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
@@ -55,6 +56,7 @@ const App = () => {
             }
           />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
