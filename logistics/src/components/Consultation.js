4577879b12ec8db9d593d77dcbc28dd8fdce3ec2
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-10 09:31:14 -0700

    added consultation component to be used in the landing page or home component with css styling as well, plus background image for better experience

---
+++ logistics/src/components/Consultation.js
@@ -0,0 +1,15 @@
+import "../css/Consultation.css";
+
+const Consultation = () => {
+    return (
+        <div className="consult-container">
+          <div className="consult-image"></div>
+          <div className="consult-text">
+            <h2>Do you need a consultation?</h2>
+            <p>Express can give you lots of advantages, from which you will surely benefit.</p>
+            <button>Contact Us</button>
+          </div>
+        </div>
+      );
+    };
+  export default Consultation;
