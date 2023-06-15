6b7708f73f0b3546f905621dcd4227e3e2bc8a9d
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-15 15:49:03 -0700

    removed previous footer styling and restarted with a new styling option due to errors within individual components while footer was being rendered

--- logistics/src/components/Footer.js
+++ logistics/src/components/Footer.js
@@ -1,22 +1,29 @@
 import "../css/Footer.css";
 
+// function Footer() {
+//   return (
+//     <div className="footer">
+//       <div className="logo-container">
+//         {/* <Logo /> */}
+//       </div>
+//       <div className="copyright-container">
+//         <p>© {new Date().getFullYear()} A2B Link. All rights reserved.</p>
+//       </div>
+//       {/* <div className="social-media-container">
+//         <Link to="/facebook"><SocialMediaIcon type="facebook" /></Link>
+//         <Link to="/twitter"><SocialMediaIcon type="twitter" /></Link>
+//         <Link to="/linkedin"><SocialMediaIcon type="linkedin" /></Link>
+//       </div> */}
+//     </div>
+//   );
+// }
 
-function Footer() {
+const Footer = () => {
   return (
-    <div className="footer">
-      <div className="logo-container">
-        {/* <Logo /> */}
-      </div>
-      <div className="copyright-container">
-        <p>© {new Date().getFullYear()} A2B Link. All rights reserved.</p>
-      </div>
-      {/* <div className="social-media-container">
-        <Link to="/facebook"><SocialMediaIcon type="facebook" /></Link>
-        <Link to="/twitter"><SocialMediaIcon type="twitter" /></Link>
-        <Link to="/linkedin"><SocialMediaIcon type="linkedin" /></Link>
-      </div> */}
-    </div>
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Your Company. All rights reserved.</p>
+    </footer>
   );
-}
+};
 
 export default Footer;
