d2ef3da40c0d8c0c121725f0735dfff68769ccd4
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-15 09:30:55 -0700

    added asset/image to consultation with initial mockup of component with proper styling

--- logistics/src/components/Consultation.js
+++ logistics/src/components/Consultation.js
@@ -1,13 +1,22 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "../css/Consultation.css";
 
-const Consultation = ({ onContactUsClick }) => {
+const Consultation = () => {
+
+  const navigate = useNavigate()
+
+  const handleOnClick = () => {
+    navigate("/contact")
+  }
+
     return (
         <div className="consult-container">
           <div className="consult-image"></div>
           <div className="consult-text">
             <h2>Do you need a consultation?</h2>
-            <p>Express can give you lots of advantages, from which you will surely benefit.</p>
-            <button onClick={onContactUsClick}>Contact Us</button>
+            {/* <p>Express can give you lots of advantages, from which you will surely benefit.</p> */}
+            <button onClick={handleOnClick}>Contact Us</button>
           </div>
         </div>
       );
