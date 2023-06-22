e90ee3458285e71a35cbda286b70202a1e264fd3
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-21 20:17:57 -0700

    updated contact route's css with styling/margin, colors with a unpacking video to share the container with proper mobile view viewing with flex

--- logistics/src/components/Contact.js
+++ logistics/src/components/Contact.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ContactForm from "./ContactForm";
 import "../css/Contact.css";
-
+import Unpacking from "../assets/Unpacking.mp4";
 
 const Contact = () => {
-   
-    return (
+  const videoRef = useRef();
+
+  // video should autoplay across all devices and screen sizes
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, []);
+
+  const videoStyles = {
+    width: "100%",
+    height: "100%",
+  };
+
+  return (
     <div className="contact-container">
-      <ContactForm />
+      <div className="contact-form-container">
+        <ContactForm />
+      </div>
+      <div className="packaging-container">
+        <video
+          ref={videoRef}
+          src={Unpacking}
+          // autoPlay="autoplay"
+          playsInLine="playsinline"
+          loop="true"
+          muted="true"
+          style={videoStyles}
+        />
+        <div className="video-text">
+          <p>How can we help you?</p>
+        </div>
+      </div>
     </div>
-    
-);
+  );
 };
 
 export default Contact;
