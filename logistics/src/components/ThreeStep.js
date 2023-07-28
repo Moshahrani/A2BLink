7eed1f3722c75a6af32df959080c6bb64c90f1b7
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-07-27 17:49:19 -0700

    updated three step process with new and improved statements from client for better representation of services to customers

--- logistics/src/components/ThreeStep.js
+++ logistics/src/components/ThreeStep.js
@@ -5,34 +5,36 @@ const ThreeStep = () => {
     <div className="Threestep-component">
       <div className="Threestep-image"></div>
       <div className="Threestep-container">
-        <h2 className="Threestep-intro">One unified marketplace. Various modes for what you need.</h2>
+        <h2 className="Threestep-intro">
+          One unified marketplace. Various modes for what you need.
+        </h2>
         <h2>Solution in 3 Easy Steps </h2>
         <div className="sub-container-wrapper">
           <div className="sub-container">
             <div className="step">1</div>
             <div className="sub-content">
-              <h3>Detail Your Requirements</h3>
+              <h3>Requirements Gathering</h3>
               <p>
-                Identify your freight specifics including goods type, volume,
-                weight, locations, and handling instructions.
+                We identify your freight requirements including type, volume,
+                weight, locations, and handling.
               </p>
             </div>
           </div>
           <div className="sub-container">
             <div className="step">2</div>
             <div className="sub-content">
-              <h3>Contact Us</h3>
-              <p>
-                Reach us via our website, email, or phone with the details of
-                your freight requirements
-              </p>
+              <h3>Carrier Match</h3>
+              <p>We use our network of the top 1% of carriers.</p>
             </div>
           </div>
           <div className="sub-container">
             <div className="step">3</div>
             <div className="sub-content">
-              <h3>Get Your Plan</h3>
-              <p>Receive a personalized service plan tailored to your needs.</p>
+              <h3>End to End Solution</h3>
+              <p>
+                We manage the documentation, transportation, tracking, and final
+                delivery of your freight.
+              </p>
             </div>
           </div>
         </div>
