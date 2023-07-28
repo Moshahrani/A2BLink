f445f36db24990af92138f84c94d3e55f4deb1e8
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-07-28 14:05:23 -0700

    inital mockup of facts insights component set

---
+++ logistics/src/components/FactsInsights.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FactsInsights = () => {
+  return (
+    <div className="facts-container">
+      <div className="fact-box">
+        <span></span>
+        <p></p>
+      </div>
+      <div className="fact-box">
+        <span></span>
+        <p></p>
+      </div>
+      <div className="fact-box">
+        <span></span>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default FactsInsights;
