5989dc0f207d6c6140c96b610bf69e2a533a53f2
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-07-28 18:40:01 -0700

    containers, statements, information setup properly within component for a smooth design

--- logistics/src/components/FactsInsights.js
+++ logistics/src/components/FactsInsights.js
@@ -1,22 +1,33 @@
 import React from "react";
+import "../css/FactsInsights.css";
 
 const FactsInsights = () => {
-  return (
-    <div className="facts-container">
-      <div className="fact-box">
-        <span></span>
-        <p></p>
+    return (
+      <div className="facts-container">
+        <h1>Solution in 3 Easy Steps</h1>
+        <div className="fact-box-container">
+          <div className="fact-box">
+            <span>Requirements Gathering</span>
+            <p>
+              We identify your freight requirements including type, volume,
+              weight, locations, and handling.
+            </p>
+          </div>
+          <div className="fact-box">
+            <span>Carrier Match</span>
+            <p>We use our network of the top 1% of carriers.</p>
+          </div>
+          <div className="fact-box">
+            <span>End to End Solution</span>
+            <p>
+              We manage the documentation, transportation, tracking, and final
+              delivery of your freight.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="fact-box">
-        <span></span>
-        <p></p>
-      </div>
-      <div className="fact-box">
-        <span></span>
-        <p></p>
-      </div>
-    </div>
-  );
-};
+    );
+  };
+  
 
 export default FactsInsights;
