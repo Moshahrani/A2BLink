d6d3ef91cf63ce4fe4475ba19cebf526fc512c63
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-07-29 07:11:18 -0700

    removed container prop from imports

--- logistics/src/components/CustomNavbar.js
+++ logistics/src/components/CustomNavbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import "../css/CustomNavbar.css";
 import resized from "../assets/resized.png";
